2aba0499dfb8a7c77505392544f576bbf87097b8
Ankit Kathayat <93024110+iamankitkathayat@users.noreply.github.com>
2022-09-25 23:52:49 +0530

    Update Ballot.sol

--- Ballot.sol
+++ Ballot.sol
@@ -1,1 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.6.0;
 
+contract Ballot {
+    struct Voter {
+        uint weight;
+        bool voted;
+        address delegate;
+        uint vote;
+    }
+    
+
+
+}
