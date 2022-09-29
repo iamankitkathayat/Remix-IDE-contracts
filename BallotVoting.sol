87eba574fe16181ec91aa208ea83fd562ebdf0aa
Ankit Kathayat <93024110+iamankitkathayat@users.noreply.github.com>
2022-09-29 21:56:14 +0530

    Update BallotVoting.sol

--- BallotVoting.sol
+++ BallotVoting.sol
@@ -69,4 +69,5 @@ contract BallotVoting {
         Voter storage sender = voters[msg.sender];
         require(sender.weight != 0, "Has no right to vote");
         require(!sender.voted, "Already voted.");
+        sender.voted = true;
 }
