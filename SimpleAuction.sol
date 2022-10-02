86a3c186cf0692db44db299815ed862b898f1022
Ankit Kathayat <93024110+iamankitkathayat@users.noreply.github.com>
2022-10-02 21:00:50 +0530

    Update SimpleAuction.sol

--- SimpleAuction.sol
+++ SimpleAuction.sol
@@ -36,4 +36,19 @@ contract SimpleAuction {
         auctionEndTime = block.timestamp + biddingTime;
     }
 
+    function bid() external payable {
+        
+        if (block.timestamp > auctionEndTime)
+            revert AuctionAlreadyEnded();
+
+        if (msg.value <= highestBid)
+            revert BidNotHighEnough(highestBid);
+
+        if (highestBid != 0) {
+            pendingReturns[highestBidder] += highestBid;
+        }
+        highestBidder = msg.sender;
+        highestBid = msg.value;
+        emit HighestBidIncreased(msg.sender, msg.value);
+    }
 }
