60b193f15cd352dcce1e95c1bd51712004de710b
Ankit Kathayat <93024110+iamankitkathayat@users.noreply.github.com>
2022-10-07 23:43:31 +0530

    Update BlindAuction.sol

--- BlindAuction.sol
+++ BlindAuction.sol
@@ -1,1 +1,50 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.4;
+contract BlindAuction {
+    struct Bid {
+        bytes32 blindedBid;
+        uint deposit;
+    }
 
+    address payable public beneficiary;
+    uint public biddingEnd;
+    uint public revealEnd;
+    bool public ended;
+
+    mapping(address => Bid[]) public bids;
+
+    address public highestBidder;
+    uint public highestBid;
+
+    // Allowed withdrawals of previous bids
+    mapping(address => uint) pendingReturns;
+
+    event AuctionEnded(address winner, uint highestBid);
+    /// Try again at `time`.
+    error TooEarly(uint time);
+    /// The function has been called too late.
+    /// It cannot be called after `time`.
+    error TooLate(uint time);
+    /// The function auctionEnd has already been called.
+    error AuctionEndAlreadyCalled();
+
+    modifier onlyBefore(uint time) {
+        if (block.timestamp >= time) revert TooLate(time);
+        _;
+    }
+    modifier onlyAfter(uint time) {
+        if (block.timestamp <= time) revert TooEarly(time);
+        _;
+    }
+
+    constructor(
+        uint biddingTime,
+        uint revealTime,
+        address payable beneficiaryAddress
+    ) {
+        beneficiary = beneficiaryAddress;
+        biddingEnd = block.timestamp + biddingTime;
+        revealEnd = biddingEnd + revealTime;
+    }
+
+}
