1e772b52329cdf16246f7dfbdc7a5fc1f12394a9
Ankit Kathayat <93024110+iamankitkathayat@users.noreply.github.com>
2022-10-15 22:48:44 +0530

    Update CrowdFund.sol

--- CrowdFund.sol
+++ CrowdFund.sol
@@ -67,4 +67,13 @@ contract CrowdFund {
         emit Launch(count, msg.sender, _goal, _startAt, _endAt);
     }
     
+    function cancel(uint _id) external {
+        Campaign memory campaign = campaigns[_id];
+        require(campaign.creator == msg.sender, "not creator");
+        require(block.timestamp < campaign.startAt, "started");
+
+        delete campaigns[_id];
+        emit Cancel(_id);
+    }
+    
 }
